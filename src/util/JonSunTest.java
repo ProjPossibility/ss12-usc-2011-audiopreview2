f82ff90bf8249b0bdee48839e60ddfe958fe8f37
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-19 21:16:12 +0000

    jon sun

--- src/util/JonSunTest.java
+++ src/util/JonSunTest.java
@@ -6,7 +6,5 @@ public class JonSunTest
 	public static void main(String [] args)
 	{
 		System.out.println("Jon Sun can commit!!!");
-		
-		System.out.println("Andy Ybarra can commit also, but only under Jon's name :(");
 	}
 }
