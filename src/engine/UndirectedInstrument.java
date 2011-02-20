88ddbbb989db7c2512d68f95262fd0c59162b12e
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-20 00:59:49 +0000

    Adding UndirectedInstrument.
    -Brehon Humphrey

---
+++ src/engine/UndirectedInstrument.java
@@ -0,0 +1,38 @@
+package engine;
+
+import javax.vecmath.Point3d;
+/**
+ * Object for instruments that produce undirected sound e.g. strings and woodwinds
+ * 
+ * @author Brehon Humphrey
+ *
+ */
+public class UndirectedInstrument extends Instrument {
+
+
+	
+	/**
+	 * Default constructor
+	 */
+	public UndirectedInstrument()
+	{
+		super();
+	}
+	
+	/**
+	 * Constructs a new Instrument
+	 */
+	public UndirectedInstrument(String name, int channelNum,int channelVol, Point3d location)
+	{
+		super(name,channelNum,channelVol,location);
+	}
+	
+
+
+	@Override
+	public double getAdjustedVolume(SeatSection s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
