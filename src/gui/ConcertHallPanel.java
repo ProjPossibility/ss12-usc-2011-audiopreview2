d01cb71af7df1a21a44e4e026bb0769f12d9615d
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-20 01:49:47 +0000

    ZACH some gui stuff

--- src/gui/ConcertHallPanel.java
+++ src/gui/ConcertHallPanel.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -30,10 +32,17 @@ public class ConcertHallPanel extends JPanel implements KeyListener
 	{
 		bgrect = new Rectangle(0,0,WINDOW_WIDTH,WINDOW_WIDTH);
 		setLayout(null);
-		JButton b = new JButton("click here");
+		
+		JButton b = new JButton("");
 		b.setLocation(0, 0);
 		b.setSize(WINDOW_WIDTH, WINDOW_HEIGHT / 2);
+		
+		b.setBackground(Color.white);
+		b.setOpaque(true);
+		b.setBorderPainted(false);
 		add(b);
+		
+		setUpSectionGroups();
 	}
 	
 	public void setUpSectionGroups()
@@ -54,11 +63,35 @@ public class ConcertHallPanel extends JPanel implements KeyListener
 		
 		currentSectionSelected = 1;
 		
-		for (int i = 0; i < sectionGroups.size(); i++)
+		int i = 0;
+		for ( i = 0; i < sectionGroups.size(); i++)
 		{
 			if (i <= lastLeftSectionGroup)
 			{
-				sectionGroups.get(i).setLocation(0, 0);
+				int height = 50;
+				if (i == lastLeftSectionGroup)
+					height = 275;
+				sectionGroups.get(i).setLocation(100, height);
+				
+			}
+			else if (i <= lastMidSectionGroup)
+			{
+				int height = 250;
+				if (i == lastLeftSectionGroup + 2)
+					height = 375;
+				if (i == lastMidSectionGroup)
+					height = 500;
+				sectionGroups.get(i).setLocation(250, height);
+			}
+			else
+			{
+				int height = 50;
+				if (i == lastRightSectionGroup)
+					height = 275;
+				if (i == sectionGroups.size() - 1)
+					sectionGroups.get(i).setLocation(600, height);
+				else
+					sectionGroups.get(i).setLocation(600, height);
 			}
 		}
 		
@@ -76,16 +109,61 @@ public class ConcertHallPanel extends JPanel implements KeyListener
 		switch(keypressed)
 		{
 			case KeyEvent.VK_UP:
+				switch(currentSectionSelected)
+				{
+					case 1:
+						currentSectionSelected = 0;
+						break;
+					case 3:
+						currentSectionSelected = 2;
+						break;
+					case 4:
+						currentSectionSelected = 3;
+						break;
+					case 6:
+						currentSectionSelected = 5;
+						break;
+				}
 				updateButtonGroupDisplays();
 				break;
 			case KeyEvent.VK_DOWN:
+				switch(currentSectionSelected)
+				{
+					case 0:
+						currentSectionSelected = 1;
+						break;
+					case 2:
+						currentSectionSelected = 3;
+						break;
+					case 3:
+						currentSectionSelected = 4;
+						break;
+					case 5:
+						currentSectionSelected = 6;
+						break;
+				}
 				updateButtonGroupDisplays();
 				break;
 			case KeyEvent.VK_LEFT:
+				if (currentSectionSelected > lastMidSectionGroup)   // if in the right side
+				{
+					currentSectionSelected = lastMidSectionGroup;  //go to the middle
+				}
+				else if (currentSectionSelected > lastLeftSectionGroup) //else if in the middle
+				{
+					currentSectionSelected = lastLeftSectionGroup; //go to the left
+				}
 				updateButtonGroupDisplays();
 				break;
 			case KeyEvent.VK_RIGHT:
-				
+				if (currentSectionSelected <= lastLeftSectionGroup)  //if in the left side
+				{
+					currentSectionSelected = lastMidSectionGroup;  //go to the middle
+				}
+				else if (currentSectionSelected <= lastMidSectionGroup)  //else if in the middle
+				{
+					currentSectionSelected = lastRightSectionGroup; //go to the right
+				}
 				updateButtonGroupDisplays();
 				break;
 		}
