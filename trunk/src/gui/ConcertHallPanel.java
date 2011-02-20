eae7b21dadc798690837aa1ffd8a292977805d94
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-20 01:04:32 +0000

    ZACH haven't updated in a while, gonnna risk one

--- trunk/src/gui/ConcertHallPanel.java
+++ trunk/src/gui/ConcertHallPanel.java
@@ -1,60 +1,117 @@
 package gui;
 
-import java.applet.Applet;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ConcertHallPanel extends JPanel 
+public class ConcertHallPanel extends JPanel implements KeyListener
 {
+	
+	/**
+	 * Width of the JPanel
+	 */
 	public int WINDOW_WIDTH = 800;
+	/**
+	 * Height of JPanel
+	 */
 	public int WINDOW_HEIGHT = 600;
 	Rectangle bgrect;
+	List<JButtonGroup> sectionGroups;
+	int lastLeftSectionGroup;
+	int lastMidSectionGroup;
+	int currentSectionSelected;
+	int lastRightSectionGroup;
+	
 	public  ConcertHallPanel()
 	{
 		bgrect = new Rectangle(0,0,WINDOW_WIDTH,WINDOW_WIDTH);
-		//setBounds(50,50, WINDOW_WIDTH, WINDOW_HEIGHT);
-
-		setLayout(new GridLayout(1,1));
 		setLayout(null);
 		JButton b = new JButton("click here");
-		//b.setLocation(20, 30);
-		add(b);
-		System.out.println("button added");
+		b.setLocation(0, 0);
 		b.setSize(WINDOW_WIDTH, WINDOW_HEIGHT / 2);
-		
+		add(b);
 	}
 	
+	public void setUpSectionGroups()
+	{
+		sectionGroups = new ArrayList<JButtonGroup>();
+		sectionGroups.add( new JButtonGroup(this, 2, State.NOT_HOVERING, 2));
+		sectionGroups.add( new JButtonGroup(this, 2, State.NOT_HOVERING, 3));
+		lastLeftSectionGroup = 1;
+		
+		sectionGroups.add(  new JButtonGroup(this, 3, State.HOVERING, 1));
+		sectionGroups.add( new JButtonGroup(this, 3, State.NOT_HOVERING, 2));
+		sectionGroups.add(  new JButtonGroup(this, 3, State.NOT_HOVERING, 3));
+		lastMidSectionGroup = 4;
+		
+		sectionGroups.add(  new JButtonGroup(this, 2, State.NOT_HOVERING, 2));
+		sectionGroups.add(  new JButtonGroup(this, 1, State.NOT_HOVERING, 3));
+		lastRightSectionGroup = 6;
+		
+		currentSectionSelected = 1;
+		
+		for (int i = 0; i < sectionGroups.size(); i++)
+		{
+			if (i <= lastLeftSectionGroup)
+			{
+				sectionGroups.get(i).setLocation(0, 0);
+			}
+		}
+		
+		
+	}
 	/*public void actionPerformed( ActionEvent ae ) {  //called every time the timer fires, updates and redraws the game
 		    
 		repaint();
 	}*/
-	
-	/*public static void main(String [] args)
-	{
-		ConcertHallLayout c = new ConcertHallLayout();
-		c.setSize( c.WINDOW_WIDTH,c.WINDOW_HEIGHT);	
-		c.setVisible(true);
-				c.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	}*/
 
-	/*public void paint (Graphics g)
-	{
-		Graphics2D g2 = (Graphics2D)g;
-		g2.setColor( Color.red );
-		g2.fill( bgrect );
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		int keypressed = event.getKeyCode();
+		switch(keypressed)
+		{
+			case KeyEvent.VK_UP:
+				updateButtonGroupDisplays();
+				break;
+			case KeyEvent.VK_DOWN:
+				updateButtonGroupDisplays();
+				break;
+			case KeyEvent.VK_LEFT:
+				updateButtonGroupDisplays();
+				break;
+			case KeyEvent.VK_RIGHT:
+				
+				updateButtonGroupDisplays();
+				break;
+		}
+		
+	}
 
-	}*/
+	private void updateButtonGroupDisplays()
+	{
+		for (int i = 0; i < sectionGroups.size(); i++)
+		{
+			sectionGroups.get(i).setGroupsIcon(State.NOT_HOVERING);
+		}
+		sectionGroups.get(currentSectionSelected).setGroupsIcon(State.HOVERING);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
