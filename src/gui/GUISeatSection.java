ac8a4bf453bce1d02633252c1206c73b317cb1ed
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-20 19:56:12 +0000

    adding images to the buttons and documenting class

--- src/gui/GUISeatSection.java
+++ src/gui/GUISeatSection.java
@@ -1,10 +1,11 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.vecmath.Point3d;
@@ -12,57 +13,139 @@ import engine.SeatSection;
 
 enum State {NOT_SELECTED,SELECTED};
 
+/**
+ * GUISeatSection.java
+ * Purpose: Takes the row and column values and map them to the correct 
+ * SeatSection objects by finding the corresponding name and vector components
+ * @author Andy Ybarra
+ *
+ */
 public class GUISeatSection extends JButton implements ActionListener{
-	SeatSection mySeatSection;
-	Point3d location;
-	File voiceFile;
-	State GSS_state;
-	ImageIcon redSeats;
-	ImageIcon blueSeats;
-	ConcertHallPanel parent;
 	
+	/**	Own SeatSection variable to be instantiated later*/
+	SeatSection mySeatSection;
+	
+	/**	Own Point3d location used for holding the 3 component vector values*/
+	Point3d location;
+	
+	/** File voiceFile to be loaded and passed to the SeatSection */
+	File voiceFile;
+	
+	/** Current enum state of the button */
+	State GSS_state;
+	
+	/** Image icon for the seat */
+	ImageIcon seatIcon;
+	
+	/** Parent panel to report button clicks to*/
+	ConcertHallPanel parentPanel;
+	
+	/** Image to put on the ImageIcon for selected states*/
+	Image redSeat;
+	
+	/** Image to put on the ImageIcon for not selected states*/
+	Image blueSeat;
+	
+	/**
+	 * 
+	 * @param Parent panel used so we can report to button clicks.
+	 * @param row int used for mapping the location to the actual GUI.
+	 * @param col int used for mapping the location to the actual GUI.
+	 */
 	public GUISeatSection(ConcertHallPanel p,int row, int col){
-		loadImages();
-		parent = p;
+		parentPanel = p;
 		String name = lookUpNameAndLocation(row, col);
 		mySeatSection = new SeatSection(location, name, voiceFile);
 		GSS_state = State.NOT_SELECTED;
 		addActionListener(this);
+		seatIcon = new ImageIcon();
+		loadImages();
+		setGSS_Color(GSS_state);
 	}
 	
+	/**
+	 * Loads the two images using the grabImage method to get the images
+	 * 
+	 * @param none
+	 */
 	public void loadImages(){
-		
+		redSeat = grabImage("..\\images\\seatButton.png");
+		blueSeat = grabImage("..\\images\\seatButtonSelected.png");
 	}
 	
+	/**
+	 * Gets the Image from the local directory and returns the image
+	 * 
+	 * @param String filename in directory.
+	 */
+	public Image grabImage(String filename){
+		File sourceimage = new File(filename);
+    	Image toReturn;
+        toReturn = Toolkit.getDefaultToolkit().getImage(filename);
+		return toReturn;
+	}
+	
+	/**
+	 * 
+	 * @return the current 
+	 */
 	public State getState(){
 		return GSS_state;
 	}
+	
+	/**
+	 * Sets the State based on the State passed in.
+	 * @param selectedOrNot
+	 */
 	public void setState(State selectedOrNot){
 		GSS_state = selectedOrNot;
 	}
+	
+	/**
+	 * 
+	 * @return the SeatSection object to the caller
+	 */
 	public SeatSection getSeatSection(){
 		return mySeatSection;
 	}
 	
+	/**
+	 * Sets the seatIcon ImageIcon based on the State enum passed in.
+	 * @param selectedOrNot
+	 */
 	public void setGSS_Color(State selectedOrNot){
 		switch(selectedOrNot){
+		/** This will display the blue seats  */
 		case NOT_SELECTED:
-			//will have red chains image icon.
 			this.setBackground(Color.BLUE);
-			this.setForeground(Color.BLUE);
-			this.setBorderPainted(false);
-			this.setOpaque(true);
+			seatIcon.setImage(blueSeat);
+			this.setIcon(seatIcon);
+			//will have red chains image icon.
+			//this.setBackground(Color.BLUE);
+			//this.setForeground(Color.BLUE);
+			//this.setBorderPainted(false);
+			//this.setOpaque(true);
 			break;
+		/** This will set the ImageIcon to it's corresponding ConcertHall Image*/
 		case SELECTED:
-			//will have the blue chains image icon.
 			this.setBackground(Color.RED);
-			this.setForeground(Color.RED);
-			this.setBorderPainted(false);
-			this.setOpaque(true);
+			seatIcon.setImage(redSeat);
+			this.setIcon(seatIcon);
+			//will have the blue chains image icon.
+			///this.setBackground(Color.RED);
+			//this.setForeground(Color.RED);
+			///this.setBorderPainted(false);
+			//this.setOpaque(true);
 			break;
 		}
 	}
 	
+	/**
+	 * This will return the corresponding 
+	 * @param row
+	 * @param col
+	 * @return
+	 */
 	public String lookUpNameAndLocation(int row, int col){
 		String name = " ";
 		location = new Point3d();
@@ -147,9 +230,11 @@ public class GUISeatSection extends JButton implements ActionListener{
 		return name;
 	}
 
-	@Override
+	/**
+	 * 
+	 */
 	public void actionPerformed(ActionEvent arg0) {
 		System.out.println("button clicked");
-		parent.buttonClicked(this);
+		parentPanel.buttonClicked(this);
 	}
 }
