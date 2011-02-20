43b417472ab9ff2698a3c9ef70c9e139f5cc5ab3
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-20 01:55:05 +0000

    Andy commit--

--- trunk/src/gui/JButtonGroup.java
+++ trunk/src/gui/JButtonGroup.java
@@ -1,22 +1,24 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-enum State {HOVERING,NOT_HOVERING,NOT_GREYED_OUT,GREYED_OUT,SELECTED};
+enum State {HOVERING,NOT_HOVERING,GREYED_OUT,SELECTED};
 
 public class JButtonGroup extends JPanel implements ActionListener {
 	
 	ConcertHallPanel parent;
-	ImageIcon icon;
-	final int BUTTON_WIDTH = 30;
-	final int BUTTON_HEIGHT = 30;
+	//ImageIcon icon;
+	final int BUTTON_WIDTH = 100;
+	final int BUTTON_HEIGHT = 100;
 	ArrayList<JButton> group;
 	private int groupFloor;
 	
@@ -27,7 +29,7 @@ public class JButtonGroup extends JPanel implements ActionListener {
 		changePanelSize(numGroups);
 		group = new ArrayList<JButton>();
 		addButtons(numGroups);
-		setGroupsIcon(state);
+		
 	}
 	
 	public int getFloor(){
@@ -35,15 +37,15 @@ public class JButtonGroup extends JPanel implements ActionListener {
 	}
 	public void changePanelSize(int numGroups){
 		if(numGroups == 1){
-			this.setSize(30,80);
+			this.setSize(BUTTON_WIDTH,3*BUTTON_HEIGHT);
 			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		}
 		else if(numGroups == 2){
-			this.setSize(30,60);
+			this.setSize(BUTTON_WIDTH,BUTTON_HEIGHT*2);
 			this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 		}
 		else if(numGroups == 3){
-			this.setSize(90,30);
+			this.setSize(3*BUTTON_WIDTH,BUTTON_HEIGHT);
 			this.setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
 		}
 	}
@@ -70,6 +72,58 @@ public class JButtonGroup extends JPanel implements ActionListener {
 	}
 	
 	public void setGroupsIcon(State state){
-		
+		switch(state){
+		case HOVERING:
+			colorButtons(Color.BLUE);
+			break;
+		case NOT_HOVERING:
+			colorButtons(Color.CYAN);
+			break;
+		case GREYED_OUT: 
+			//greyed out
+			colorButtons(Color.GRAY);
+			break;
+		case SELECTED:
+			//red selected
+			colorButtons(Color.RED);
+			break;
+		default:
+			System.out.println("Error has occured, enum value not found");
+			break;
+		}
+	}
+	
+	public void setGroupsIcon(State state, int index){
+		if(index != -1){
+			//When you're selecting one particular subsection
+			if(state == state.SELECTED){
+				for(int j = 0; j< group.size();j++){
+					if(j != index){
+						group.get(j).setBackground(Color.CYAN);
+						group.get(j).setForeground(Color.CYAN);
+						group.get(j).setOpaque(true);
+						group.get(j).setBorderPainted(false);
+					}
+					else{
+						group.get(j).setBackground(Color.RED);
+						group.get(j).setBackground(Color.RED);
+						group.get(j).setOpaque(true);
+						group.get(j).setBorderPainted(false);
+					}
+				}
+			}
+		}
+		else{
+			setGroupsIcon(state);
+		}
+	}
+	
+	public void colorButtons(Color c){
+		for(JButton j : group){
+			j.setBackground(c);
+			j.setForeground(c);
+			j.setOpaque(true);
+			j.setBorderPainted(false);
+		}
 	}
 }
