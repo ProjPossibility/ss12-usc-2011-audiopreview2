247da236e3ce8552c1a2b879dbec054b210ff0af
donnyton@gmail.com <donnyton@gmail.com@e90c4b63-9aa4-8d4c-b6ff-4dd3ae34e00b>
2011-02-20 01:19:43 +0000

    Added protected fields to instrument Added directed instrument

--- trunk/src/engine/DirectedInstrument.java
+++ trunk/src/engine/DirectedInstrument.java
@@ -1,17 +1,27 @@
 package engine;
 
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
 public class DirectedInstrument extends Instrument
 {
-	public DirectedInstrument()
+	public static final int DISTANCE_DROP_FACTOR = 1;
+	
+	protected Vector3d iDirection;
+	protected double iForwardPortion;
+	
+	public DirectedInstrument(String name, int channelNum, int channelVol, 
+		Point3d location, Vector3d direction, double forwardPortion)
 	{
-		
+		super(name, channelNum, channelVol, location);
+		iDirection = direction;
+		iForwardPortion = forwardPortion;
 	}
 
-	@Override
 	public double getAdjustedVolume(SeatSection s)
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return channelVolume / 
+			Math.pow(iLocation.distance(s.getLocation()), DISTANCE_DROP_FACTOR);
 	}
 
 }
